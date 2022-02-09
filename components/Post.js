a3bfde0a94edee93b0760b2feb4e6bd6eeac2f5f
taniguchi <ta22guchi@gmail.com>
2022-02-09 09:55:07 +0900

    linkが使えないので一旦aで

--- components/Post.js
+++ components/Post.js
@@ -1,14 +1,13 @@
-import Link from "next/link";
 export default function Post({ post }) {
   return (
-    <div>
+    <a>
       <span>{post.id}</span>
       {" : "}
-      <Link href={`/posts/${post.id}`} passHref>
+      <a href={`/posts/${post.id}`}>
         <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
           {post.title}
         </span>
-      </Link>
-    </div>
+      </a>
+    </a>
   );
 }
